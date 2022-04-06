e3d95a9ad27bbd2b13ed7f1f4bb85a7f3e0d859c
JonathanWbn <jwieben@hey.com>
2022-04-06 22:13:33 +0200

    chore: use mget over get

--- repository/UpstashGameRepository.ts
+++ repository/UpstashGameRepository.ts
@@ -3,7 +3,7 @@ import { v4 as uuid } from "uuid";
 
 import { Game, IGame, Team } from "../domain/Game";
 
-const { set, scan, get, del } = Redis.fromEnv();
+const { set, scan, del, mget } = Redis.fromEnv();
 
 export class UpstashGameRepository {
   public async create(winnerTeam: Team, loserTeam: Team) {
@@ -17,29 +17,13 @@ export class UpstashGameRepository {
   }
 
   public async listAll() {
-    console.log("listAll games");
     const [, keys] = await scan(0, { match: "GAME#*", count: 1000 });
-    console.log("game keys", keys.length);
 
-    return Promise.all(
-      keys.map(async (key: string) => {
-        const data = await promiseTimeout(3000, get(key));
-        const { id, createdAt, winnerTeam, loserTeam } = data as IGame;
-        return new Game(id, new Date(createdAt), winnerTeam, loserTeam);
-      })
-    );
+    const games = await mget(keys[0], ...keys.slice(1));
+
+    return games.map((data) => {
+      const { id, createdAt, winnerTeam, loserTeam } = data as IGame;
+      return new Game(id, new Date(createdAt), winnerTeam, loserTeam);
+    });
   }
-}
-
-function promiseTimeout<T>(ms: number, promise: Promise<T>): Promise<T> {
-  // Create a promise that rejects in <ms> milliseconds
-  let timeout = new Promise<T>((resolve, reject) => {
-    let id = setTimeout(() => {
-      clearTimeout(id);
-      reject("Timed out in " + ms + "ms.");
-    }, ms);
-  });
-
-  // Returns a race between our timeout and the passed in promise
-  return Promise.race([promise, timeout]);
 }
