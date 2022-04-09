e9d36294583f3ff835ae58715bf7d7894d5eeeca
JonathanWbn <jwieben@hey.com>
2022-04-09 10:53:01 +0200

    refactor: use mget over get

--- repository/UpstashPlayerRepository.ts
+++ repository/UpstashPlayerRepository.ts
@@ -3,7 +3,7 @@ import { v4 as uuid } from "uuid";
 
 import { IPlayer, Player, PlayerAnimal } from "../domain/Player";
 
-const { set, scan, get, del } = Redis.fromEnv();
+const { set, scan, mget, del } = Redis.fromEnv();
 
 export class UpstashPlayerRepository {
   public async create(name: string, animal: PlayerAnimal) {
@@ -23,12 +23,11 @@ export class UpstashPlayerRepository {
   public async listAll() {
     const [, keys] = await scan(0, { match: "PLAYER#*", count: 1000 });
 
-    return Promise.all(
-      keys.map(async (key: string) => {
-        const data = await get(key);
-        const { id, name, animal, isRetired } = data as IPlayer;
-        return new Player(id, name, animal, Boolean(isRetired));
-      })
-    );
+    const players = await mget(keys[0], ...keys.slice(1));
+
+    return players.map((data) => {
+      const { id, name, animal, isRetired } = data as IPlayer;
+      return new Player(id, name, animal, Boolean(isRetired));
+    });
   }
 }
